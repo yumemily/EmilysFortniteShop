c388317b754c34cb5eda81d6b5f3dca03de784a1
Emily Yum <emilyyum@gmail.com>
2020-04-05 07:24:30 -0700

    deleted blue background on homepage

--- src/App.js
+++ src/App.js
@@ -28,13 +28,11 @@ function App() {
 }
 
 const Home = () => (
-  <div style={{backgroundColor: '#065DB7'}}>
     <Jumbotron className='homepage' fluid>
       <Container>
         <h1 className='pb-5'>WELCOME TO EM'S FORTNITE SHOP</h1>
         <img className='img-fluid' src='https://img.pngio.com/spaceman-fortnite-transparent-png-stickpng-transparent-fortnite-571_724.png' alt='spaceman'/>
       </Container>
     </Jumbotron>
-  </div>
 )
 export default App;
