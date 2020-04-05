20cd1ebbafbfa555bed9a74d97206a2d35938a0f
Emily Yum <emilyyum@gmail.com>
2020-04-05 02:16:51 -0700

    fixed spinner position

--- src/MySpinner.js
+++ src/MySpinner.js
@@ -3,14 +3,14 @@ import Spinner from 'react-bootstrap/Spinner'
 
 export default function MySpinner() {
     return (
-        <div>
+        <div class='container d-flex justify-content-center'>
             <Spinner animation="border"  
             style={{
             width: 200,
             height: 200,
             color: "rgb(47,210,218",
-            marginTop: "40vh",
-            marginLeft: "45vw"
+            marginTop: '3rem',
+            alignSelf: 'center'
           }}/>
         </div>
     )
